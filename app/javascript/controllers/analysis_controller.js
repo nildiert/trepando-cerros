d565afeee7cd08fd0f3d7d2671d32a895313e350
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-06-05 18:05:26 -0500

    Fix form submission loop

--- app/javascript/controllers/analysis_controller.js
+++ app/javascript/controllers/analysis_controller.js
@@ -3,8 +3,14 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["loader", "messages", "bar"]
 
+  connect() {
+    this.submitted = false
+  }
+
   submit(event) {
+    if (this.submitted) return
     event.preventDefault()
+    this.submitted = true
     this.loaderTarget.classList.remove('hidden')
     this.messagesTarget.innerHTML = ''
     this.barTarget.style.width = '0%'
