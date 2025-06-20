44857da3fda7bfe69f75e15a9510e78ec39ab5b5
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-06-20 07:02:52 -0500

    Register controllers manually

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,5 +1,13 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+import AnalysisController from "./analysis_controller"
+import DropdownController from "./dropdown_controller"
+import HelloController from "./hello_controller"
+import TabsController from "./tabs_controller"
+import CheckpointsController from "./checkpoints_controller"
 
-eagerLoadControllersFrom("controllers", application)
+application.register("analysis", AnalysisController)
+application.register("dropdown", DropdownController)
+application.register("hello", HelloController)
+application.register("tabs", TabsController)
+application.register("checkpoints", CheckpointsController)
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,5 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "xlsx", to: "https://cdn.jsdelivr.net/npm/xlsx@0.19.1/dist/xlsx.full.min.js"
