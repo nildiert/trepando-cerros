29ed33c7de8ff829a78da755c1a0df2194a86026
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-06-20 06:42:41 -0500

    Fix Stimulus loading and use icons

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,14 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+
+import AnalysisController from "./analysis_controller"
+import DropdownController from "./dropdown_controller"
+import HelloController from "./hello_controller"
+import TabsController from "./tabs_controller"
+import CheckpointsController from "./checkpoints_controller"
+
+application.register("analysis", AnalysisController)
+application.register("dropdown", DropdownController)
+application.register("hello", HelloController)
+application.register("tabs", TabsController)
+application.register("checkpoints", CheckpointsController)
