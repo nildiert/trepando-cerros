0aadb57933da689efceb4bec753991be9113d5d0
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-06-20 08:51:13 -0500

    Use Tailwind modal for start time

--- app/javascript/controllers/analysis_controller.js
+++ app/javascript/controllers/analysis_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import Swal from "sweetalert2"
 
 export default class extends Controller {
   static targets = [
@@ -9,7 +8,9 @@ export default class extends Controller {
     "loader",
     "messages",
     "bar",
-    "timeField"
+    "timeField",
+    "timeModal",
+    "timeInput"
   ]
 
   connect() {
@@ -20,32 +21,18 @@ export default class extends Controller {
     if (this.submitted) return
     event.preventDefault()
     this.submitted = true
-    Swal.fire({
-      title: 'Selecciona tu hora de inicio',
-      html: `
-        <div data-controller="time-dial" class="flex items-center space-x-4 justify-center">
-          <input id="startTimeInput" type="time" data-time-dial-target="input" class="hidden">
-          <div data-time-dial-target="dial" class="relative w-24 h-24 rounded-full border-2 border-blue-500 bg-white" data-action="pointerdown->time-dial#pointerDown pointermove->time-dial#pointerMove pointerup->time-dial#pointerUp pointerleave->time-dial#pointerUp">
-            <div data-time-dial-target="hourHand" class="absolute left-1/2 top-1/2 w-0.5 bg-blue-700 origin-bottom" style="height:30%;"></div>
-            <div data-time-dial-target="minuteHand" class="absolute left-1/2 top-1/2 w-0.5 bg-blue-400 origin-bottom" style="height:45%;"></div>
-          </div>
-          <span data-time-dial-target="label" class="text-blue-700 font-medium">00:00</span>
-        </div>
-      `,
-      confirmButtonText: 'Aceptar',
-      showCancelButton: true,
-      focusConfirm: false,
-      preConfirm: () => {
-        return document.getElementById('startTimeInput').value
-      }
-    }).then(result => {
-      if (result.isConfirmed) {
-        this.timeFieldTarget.value = result.value
-        this.startSteps()
-      } else {
-        this.submitted = false
-      }
-    })
+    this.timeModalTarget.classList.remove('hidden')
+  }
+
+  confirmTime() {
+    this.timeFieldTarget.value = this.timeInputTarget.value
+    this.timeModalTarget.classList.add('hidden')
+    this.startSteps()
+  }
+
+  cancelTime() {
+    this.timeModalTarget.classList.add('hidden')
+    this.submitted = false
   }
 
 
