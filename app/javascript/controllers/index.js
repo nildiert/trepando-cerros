a02abe68fb9dac27a7896ba35217334048734d41
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-06-20 06:57:06 -0500

    Restore Stimulus autoloading

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,14 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-import AnalysisController from "./analysis_controller"
-import DropdownController from "./dropdown_controller"
-import HelloController from "./hello_controller"
-import TabsController from "./tabs_controller"
-import CheckpointsController from "./checkpoints_controller"
-
-application.register("analysis", AnalysisController)
-application.register("dropdown", DropdownController)
-application.register("hello", HelloController)
-application.register("tabs", TabsController)
-application.register("checkpoints", CheckpointsController)
+eagerLoadControllersFrom("controllers", application)
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,9 +3,6 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
-# The Stimulus autoloading script caused 404 errors when modules
-# were fetched dynamically. Controllers are registered manually,
-# so we no longer need to pin the loader script.
-# pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "xlsx", to: "https://cdn.jsdelivr.net/npm/xlsx@0.19.1/dist/xlsx.full.min.js"
