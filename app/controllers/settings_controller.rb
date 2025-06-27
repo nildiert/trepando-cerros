0407bfc87a6efcd0fe43b95d3d2ea677386a3e06
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-06-26 23:58:50 -0500

    Nest settings page under athlete dashboard

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,6 @@
 class SettingsController < ApplicationController
   before_action :authenticate_user
+  before_action :set_athlete
 
   def show
     authorize! :manage, :settings
@@ -11,12 +12,28 @@ class SettingsController < ApplicationController
     perm = current_user.permissions.find_or_initialize_by(name: 'race_predictor')
     perm.enabled = params[:race_predictor] == '1'
     perm.save!
-    redirect_to settings_path, notice: 'Configuraci\u00f3n actualizada'
+    redirect_to athlete_settings_path(@athlete), notice: 'Configuraci\u00f3n actualizada'
   end
 
   private
 
   def authenticate_user
     redirect_to root_path unless session[:user_id]
+  end
+
+  def set_athlete
+    @athlete = fetch_athlete(params[:athlete_id])
+  end
+
+  def fetch_athlete(id = nil)
+    token = session[:strava_token] || ENV['STRAVA_ACCESS_TOKEN']
+    client = StravaClient.new(access_token: token)
+    if id.present? && current_athlete_id.present? && id.to_s != current_athlete_id.to_s
+      client.athlete(id)
+    else
+      client.athlete
+    end
+  rescue StandardError
+    nil
   end
 end
