763f30a613985e7401af80c803ff4f0e1df26fb3
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-07-12 21:22:19 -0500

    Show sidebar on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,25 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user
+  before_action :set_athlete
 
   def show
-    # placeholder view, intentionally left blank
+  end
+
+  private
+
+  def set_athlete
+    @athlete = fetch_athlete(params[:athlete_id])
+  end
+
+  def fetch_athlete(id = nil)
+    token = session[:strava_token] || ENV['STRAVA_ACCESS_TOKEN']
+    client = StravaClient.new(access_token: token)
+    if id.present? && current_athlete_id.present? && id.to_s != current_athlete_id.to_s
+      client.athlete(id)
+    else
+      client.athlete
+    end
+  rescue StandardError
+    nil
   end
 end
