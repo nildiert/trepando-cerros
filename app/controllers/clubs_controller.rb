0bf04321e564770bb19357a367459c7b7ae66b8e
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-08-01 07:53:39 -0500

    Add club creation and modern listing

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -5,4 +5,25 @@ class ClubsController < ApplicationController
   def index
     @clubs = Club.all
   end
+
+  def new
+    authorize! :manage, Club
+    @club = Club.new
+  end
+
+  def create
+    authorize! :manage, Club
+    @club = Club.new(club_params)
+    if @club.save
+      redirect_to clubs_path, notice: 'Club creado'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def club_params
+    params.require(:club).permit(:name, :description)
+  end
 end
