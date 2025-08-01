6182c5093129aac8948b0c6ec604c12ee0ff9795
Nildiert Jimenez <39206686+nildiert@users.noreply.github.com>
2025-08-01 08:38:22 -0500

    Propagate role permission changes to users

--- app/models/role_permission.rb
+++ app/models/role_permission.rb
@@ -4,4 +4,15 @@ class RolePermission < ApplicationRecord
   AVAILABLE_PERMISSIONS = %w[race_predictor training_plan club athletes users].freeze
 
   validates :name, presence: true
+
+  after_commit :propagate_to_users
+
+  private
+
+  def propagate_to_users
+    role.users.find_each do |user|
+      perm = user.permissions.find_or_initialize_by(name: name)
+      perm.update(enabled: enabled)
+    end
+  end
 end
